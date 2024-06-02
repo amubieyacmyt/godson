688b88bcf1816ae14ec6ca0d71d48c3ce6876082
amubieyacmyt <elijahamubieya@gmail.com>
2024-06-02 21:49:52 +0100

    Initial commit

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML  stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language"
+    ]
+    },
+    {
+        numb: 2,
+        question: "What is CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+    
+        ]
+        },
+        {
+            numb: 3,
+            question: "What is PHP stand for",
+            answer: "A. Hypertext Preprocessor",
+            options: [
+                "A. Hypertext Preprocessor",
+                "B. Hometext Programing",
+                "C. Hypertext Preprograming",
+                "D. Programing Hypertext Preprocessor"
+            ]
+            },
+            {
+                numb: 4,
+                question: "What is SQL stand for",
+                answer: "D. Structured Query Language",
+                options: [
+                    "A. Strength Query Language",
+                    "B. Stylesheet Query Language",
+                    "C. Science Question Language",
+                    "D. Structured Query Language"
+                ]
+                },
+                {
+                    numb: 5,
+                    question: "What is XMl stand for",
+                    answer: "D. Extensible Markup Language",
+                    options: [
+                        "A. Excellent Multiple Language",
+                        "B. Explore Multiple Language",
+                        "C. Extra Multiple Language",
+                        "D. Extensible Markup Language"
+
+                    ]
+                    }
+];
